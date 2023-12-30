1f777c1b62baa669d96276d14554baea62b6386a
Sourav Saha <dev.souravsaha@gmail.com>
2023-12-30 16:01:53 +0530

    Dev- changed db url

--- src/db/connectDataBase.js
+++ src/db/connectDataBase.js
@@ -3,14 +3,11 @@ const { default: mongoose } = require("mongoose");
 
 
 let connectionUrl = process.env.DB_CONNECTION_URL;
-connectionUrl = connectionUrl.replace('<username>', process.env.DB_USER_NAME);
-connectionUrl = connectionUrl.replace('<password>', process.env.DB_PASSWORD);
-// connectionUrl = `${connectionUrl}/${process.env.DB_NAME}?${process.env.DB_URL_QUERY}`;
+connectionUrl = `${connectionUrl}/${process.env.DB_NAME}?${process.env.DB_URL_QUERY}`;
 
 
 const connectDataBase = async () => {
-  await mongoose.connect(connectionUrl, { dbName: process.env.DB_NAME });
-  console.log('Database Connection Successful... 😌 ');
+  await mongoose.connect(`mongodb://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@mongodb:27017/${process.env.DB_NAME}?authSource=admin`);  
 };
 
 module.exports = connectDataBase;
