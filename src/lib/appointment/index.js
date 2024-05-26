131c1baacfa66f5e471b81f53709254f20daafa4
Sourav Saha <dev.souravsaha@gmail.com>
2024-05-26 11:49:18 +0530

    Dev- updated

--- src/lib/appointment/index.js
+++ src/lib/appointment/index.js
@@ -7,9 +7,8 @@ const completeProcedure = require('./completeProcedure');
 const updateItem = require('./updateItem');
 const deleteItem = require('./deleteItem');
 const itemExist = require('./itemExist');
-const findItemByuserId = require('./findItemByuserId');
+const findItemByuserId = require('./findItemByUserId');
 const availability = require('./availability');
-// const findSingleItemByProperty = require('./findSingleItemByProperty');
 
 module.exports = {
   availability,
