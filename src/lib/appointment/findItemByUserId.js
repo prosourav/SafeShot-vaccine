f33c3e45cfe40c63552b66691ce8bd6b1a8dc921
Sourav Saha <dev.souravsaha@gmail.com>
2024-05-26 11:38:25 +0530

    Dev- updated

--- src/lib/appointment/findItemByUserId.js
+++ src/lib/appointment/findItemByUserId.js
@@ -2,9 +2,7 @@ const Appointment = require("../../model/Appointment");
 
 
 const findItemByUserId = async (user) => {
-  // console.log('...', user)
   const appointment = await Appointment.find(user);
-  // console.log('...', appointment.length);
   return appointment ? appointment : false;
 };
 
